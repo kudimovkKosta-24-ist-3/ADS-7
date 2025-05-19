f25aa5efa72099cc26d16b7ac20fb0c585d29656
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-05-19 11:52:45 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,29 +1,24 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
+
 #include <cstdint>
+
 class Train {
  private:
-  struct Car {
-    bool light;
-    Car* next;
-    Car* prev;
-
-    explicit Car(bool l = false) : light(l), next(this), prev(this) {}
-  };
-
-  Car* first;
-  int64_t countOp;
-
+    struct Car {
+        bool light;
+        Car* next;
+        Car* prev;
+        explicit Car(bool l = false) : light(l), next(this), prev(this) {}
+    };
+    Car* first;
+    int countOp;
  public:
-  Train();
-  ~Train();
-
-  void addCar(bool light);
-
-  int64_t getLength();
-
-  int64_t getOpCount() const { return countOp; }
+    Train();
+    ~Train();
+    void addCar(bool light);
+    int getLength();
+    int getOpCount() const { return countOp; }
 };
-
 #endif  // INCLUDE_TRAIN_H_
